b850ed52ba2b363ac3203e2bf8a580ad9111033a
QRPp <awesome.walrus+github@gmail.com>
2021-10-17 02:46:30 +0100

    + Handle relayed BLE advertisements
    Add mac() per-format stage: extract MAC address from the advertisement data.
    
    Use the MAC from the advertisement data rather than the BLE source MAC for
    looking up the configured MACs.
    
    Log BLE source MAC as extra when it mismatches the MAC in the data, rather than
    the other way around.

--- src/atc_mi_decode.c
+++ src/atc_mi_decode.c
@@ -41,16 +41,16 @@ void atc_mi_set_sink(atc_mi_data_sink cb, void *opaque) {
     json_printf(out, fmt, ##__VA_ARGS__, buf);      \
   } while (0)
 
-static void maccpy_reverse(uint8_t dst[6], uint8_t src[6]) {
+static uint8_t *maccpy_reverse(uint8_t dst[6], uint8_t src[6]) {
   for (int i = 0; i < 6; i++) dst[i] = src[5 - i];
+  return dst;
 }
 
-static void mac_log_if_diff_src(struct mgos_bt_addr *src, uint8_t adv_mac[6],
+static void src_log_if_diff_mac(struct mgos_bt_addr *src, uint8_t mac[6],
                                 struct json_out *out) {
-  struct mgos_bt_addr *adv = (void *) adv_mac;
-  if (!mgos_bt_addr_cmp(src, adv)) return;
-  json_printf(out, " (%s %s)", "adv MAC",
-              mgos_bt_addr_to_str(adv, 0, alloca(MGOS_BT_ADDR_STR_LEN)));
+  if (!mac || !memcmp(src->addr, mac, sizeof(src->addr))) return;
+  const char *str = mgos_bt_addr_to_str(src, 0, alloca(MGOS_BT_ADDR_STR_LEN));
+  json_printf(out, " (%s %s)", "src", str);
 }
 
 // {{{1 ATC pvvx
@@ -73,7 +73,6 @@ static bool ble_adv_atc_pvvx_decode(struct mgos_bt_gap_scan_result *r,
                                     struct json_out *ok,
                                     struct json_out *fail) {
   struct atc_pvvx *adv = (struct atc_pvvx *) r->adv_data.p;
-  maccpy_reverse(data->mac, adv->mac);
   data->cnt = adv->cnt;
   data->temp_cC = le16toh(adv->temp_cC);
   data->humi_cPct = le16toh(adv->humi_cPct);
@@ -85,7 +84,7 @@ static bool ble_adv_atc_pvvx_decode(struct mgos_bt_gap_scan_result *r,
               data->temp_cC / 100.0, "°C", 'H', data->humi_cPct / 100.0, 'B',
               data->batt_pct, data->batt_mV, "mv", data->cnt, "flg",
               data->flags);
-  mac_log_if_diff_src(&r->addr, data->mac, ok);
+  src_log_if_diff_mac(&r->addr, data->mac, ok);
   return true;
 }
 
@@ -94,6 +93,11 @@ static bool ble_adv_atc_pvvx_detect(struct mgos_bt_gap_scan_result *r) {
   return r->adv_data.len == sizeof(*adv) && adv->hdr.len == sizeof(*adv) - 1 &&
          adv->hdr.type == MGOS_BT_GAP_EIR_SERVICE_DATA_16 &&
          le16toh(adv->uuid) == ATC_UUID;
+}
+
+static uint8_t *ble_adv_atc_pvvx_mac(struct mgos_bt_gap_scan_result *r,
+                                     struct atc_mi_data *data) {
+  return maccpy_reverse(data->mac, ((struct atc_pvvx *) r->adv_data.p)->mac);
 }
 
 // {{{1 MI encrypted
@@ -196,7 +200,6 @@ static bool ble_adv_mi_enc_decode(struct mgos_bt_gap_scan_result *r,
                       sizeof(bottom->mac_tag));
   if (pt.len != ctL - offsetof(struct mi_enc_pt, temp_dC)) goto invalid_pt;
 
-  maccpy_reverse(data->mac, top->mac);
   data->cnt = le32toh(cnt.all);
   switch (le16toh(pt.type)) {
     case 0x1004:
@@ -222,7 +225,7 @@ invalid_pt:
             ctL == sizeof(pt.batt_pct) ? pt.batt_pct : le16toh(pt.humi_dPct));
 
 success:
-  mac_log_if_diff_src(&r->addr, data->mac, ok);
+  src_log_if_diff_mac(&r->addr, data->mac, ok);
   return true;
 }
 #undef FAIL_UNLESS_MBEDTLS
@@ -243,26 +246,33 @@ static bool ble_adv_mi_enc_detect(struct mgos_bt_gap_scan_result *r) {
          le16toh(top->dev_type) == MI_ENC_DEV;
 }
 
+static uint8_t *ble_adv_mi_enc_mac(struct mgos_bt_gap_scan_result *r,
+                                   struct atc_mi_data *data) {
+  struct mi_enc_top *top = (void *) &((struct mi_enc_flags *) r->adv_data.p)[1];
+  return maccpy_reverse(data->mac, top->mac);
+}
+
 // {{{1 Dispatching
 #define FMT(name, pfx) \
-  { name, pfx##_decode, pfx##_detect }
+  { name, pfx##_decode, pfx##_detect, pfx##_mac }
 struct atc_mi_fmt {
   const char *name;
   bool (*decode)(struct mgos_bt_gap_scan_result *r, struct atc_mi *atc_mi,
                  struct atc_mi_data *data, struct json_out *ok,
                  struct json_out *fail);
   bool (*detect)(struct mgos_bt_gap_scan_result *r);
+  uint8_t *(*mac)(struct mgos_bt_gap_scan_result *r, struct atc_mi_data *data);
 } formats[] = {
     FMT("ATC/pvvx", ble_adv_atc_pvvx), FMT("MI ENC", ble_adv_mi_enc), {NULL}};
 #undef FMT
 
-static void ble_adv_log(struct mgos_bt_gap_scan_result *r,
+static void ble_adv_log(struct mgos_bt_gap_scan_result *r, const uint8_t mac[6],
                         struct atc_mi_fmt *fmt, struct atc_mi *atc_mi,
                         const char *reason, const char *detail) {
+  const struct mgos_bt_addr *addr = (void *) mac ?: &r->addr;
   LOG(LL_INFO, ("%s:%s%s (%s%s%s%s rssi %d)%s%s", reason, fmt ? " " : "",
                 fmt ? fmt->name : "",
-                mgos_bt_addr_to_str(&r->addr, MGOS_BT_ADDR_STRINGIFY_TYPE,
-                                    alloca(MGOS_BT_ADDR_STR_LEN)),
+                mgos_bt_addr_to_str(addr, 0, alloca(MGOS_BT_ADDR_STR_LEN)),
                 atc_mi && atc_mi->name ? " \"" : "",
                 atc_mi && atc_mi->name ? atc_mi->name : "",
                 atc_mi && atc_mi->name ? "\"" : "", r->rssi, detail ? ": " : "",
@@ -277,7 +287,7 @@ static void ble_adv_log_raw(struct mgos_bt_gap_scan_result *r) {
     struct mg_str name = mgos_bt_gap_parse_name(r->scan_rsp);
     if (name.p) json_printf(out, ", %s %.*Q", "name", (int) name.len, name.p);
   }
-  ble_adv_log(r, NULL, NULL, "raw", out->u.buf.buf);
+  ble_adv_log(r, NULL, NULL, NULL, "raw", out->u.buf.buf);
 }
 
 static void atc_mi_handle(int ev, void *ev_data, void *userdata) {
@@ -290,22 +300,23 @@ static void atc_mi_handle(int ev, void *ev_data, void *userdata) {
   while (fmt->name && !fmt->detect(r)) fmt++;
   if (!fmt->name) return;
 
-  struct atc_mi *atc_mi = atc_mi_find(r->addr.addr);
+  struct atc_mi_data data;
+  const uint8_t *mac = fmt->mac(r, &data);
+  struct atc_mi *atc_mi = atc_mi_find(mac ?: r->addr.addr);
   if (!atc_mi && mgos_sys_config_get_atc_mi_list_only()) {
     if (mgos_sys_config_get_atc_mi_debug_rejected())
-      ble_adv_log(r, fmt, NULL, "rejected", NULL);
+      ble_adv_log(r, mac, fmt, NULL, "rejected", NULL);
     return;
   }
 
-  struct atc_mi_data data;
   struct json_out *ok = NULL, *fail = NULL;
   if (mgos_sys_config_get_atc_mi_debug_accepted()) ok = JSON_OUT_BUFA(128);
   if (mgos_sys_config_get_atc_mi_debug_failed()) fail = JSON_OUT_BUFA(128);
   if (fmt->decode(r, atc_mi, &data, ok, fail)) {
-    if (ok) ble_adv_log(r, fmt, atc_mi, "accepted", ok->u.buf.buf);
+    if (ok) ble_adv_log(r, mac, fmt, atc_mi, "accepted", ok->u.buf.buf);
     if (sink.cb) sink.cb(r->addr.addr, atc_mi, fmt->name, &data, sink.opaque);
   } else if (fail)
-    ble_adv_log(r, fmt, atc_mi, "failed", fail->u.buf.buf);
+    ble_adv_log(r, mac, fmt, atc_mi, "failed", fail->u.buf.buf);
 }
 
 void atc_mi_decode_init() {
